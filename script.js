64b46080ab707f58c89b28f72ceb4b03ad8b74bc
paplauskis <paplauskaskostas@gmail.com>
2023-05-22 20:06:47 +0300

    added function appendGameBoard that renders the game board

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const grid = document.getElementsByClassName('grid')[0];
+
 const createGameBoard = (() => {
   let gameBoard = ['X','O','X','O','X','O','X','O','X'];
 
@@ -8,10 +10,22 @@ const createGameBoard = (() => {
   return { getGameBoard, updateBoard, resetBoard };
 })();
 
-
 const player = (name, marker) => {
   const getName = () => name;
   const getMarker = () => marker;
   const startGame = () => `${name} starts the game!`;
   return { getName, getMarker, startGame };
 }
+// const koste = player('koste', 'x')
+function appendGameBoard() {
+  const gameBoard = createGameBoard.getGameBoard();
+
+  gameBoard.forEach(marker => {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.textContent = marker;
+    grid.appendChild(cell);
+    console.log(cell);
+  })
+}
+appendGameBoard();
